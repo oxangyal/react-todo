b9673f0ac7b3a15d7f6dddd642715bcf53b0854b
oxangyal <Oxana.mitch@gmail.com>
2023-05-31 21:58:39 -0400

    Added list items

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React from 'react';
 
 function App() {
+  const todoList = [
+    {
+      id: 1,
+      title: "Open water swim 1500 yards"
+    },
+    {
+      id: 2,
+      title: "Bike ride - 15 miles"
+    },
+    {
+      id: 3,
+      title: "Bike ride - 15 miles"
+    }
+  ];
+
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <h1>ToDo List</h1>
+      <ul>
+        {todoList.map(item => (
+          <li key={item.id}> {item.title}</li>
+        )
+        )}; 
+      </ul>
+  </div>
   );
 }
 
