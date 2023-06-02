18cfb732a1830b0acf1c405289fabdc75a2034c3
oxangyal <Oxana.mitch@gmail.com>
2023-06-02 00:11:51 -0400

    Added Todo Form component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import TodoList from './TodoList';
+import AddTodoForm from './AddTodoForm';
 
 function App() {
 
   return (
     <div>
       <h1>ToDo List</h1>
+      <AddTodoForm />
       <TodoList />
     </div>
   );
