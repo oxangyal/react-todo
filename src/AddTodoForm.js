a1182c019a7b7b768e5544471e5544edaddf9c43
oxangyal <Oxana.mitch@gmail.com>
2023-06-08 14:02:20 -0400

    Control Add Todo Input

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState }  from "react";
 
 function AddTodoForm(props) {
+    const [todoTitle, setTodoTitle] = useState("");
+
+    const handleTitleChange = (event) => {
+        let newTodoTitle = event.target.value;
+        setTodoTitle(newTodoTitle);
+    };
+
     const handleAddTodo = (event) => {
         event.preventDefault();
-        let todoTitle = event.target.title.value;
-        console.log(todoTitle);
         props.onAddTodo(todoTitle);
         event.target.reset();
     };
@@ -12,7 +17,13 @@ function AddTodoForm(props) {
     return (
         <form onSubmit={handleAddTodo}>
             <label htmlFor="todoTitle">Title</label>
-            <input id="todoTitle" name="title" text="text"></input>
+            <input
+                id="todoTitle"
+                name="title"
+                type="text"
+                value={todoTitle}
+                onChange={handleTitleChange}
+            ></input>
             <button type="submit">Add</button>
         </form>
     );
