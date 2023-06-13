b7fb05051bf14b448cb3f414ecaab8269cf01ca0
oxangyal <Oxana.mitch@gmail.com>
2023-06-12 22:08:04 -0400

    Used localStorage to save Todo items

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
-import { useState } from "react";
 
 function App() {
-    const [todoList, setTodoList] = useState([]);
+    const [todoList, setTodoList] = useState(() => {
+        const savedTodoList = localStorage.getItem("savedTodoList");
+        return savedTodoList ? JSON.parse(savedTodoList) : [];
+    });
+
+    useEffect(() => {
+        localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+    }, [todoList]);
 
     const addTodo = (newTodo) => {
         setTodoList([...todoList, newTodo]);
