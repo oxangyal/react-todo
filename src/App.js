c5b57056089f42c456296c21f385b928bfa4e612
oxangyal <Oxana.mitch@gmail.com>
2023-06-22 14:23:22 -0400

    Removed custom hook and semi colon

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
 
-const useSemiPersistentState = () => {
+function App() {
     const [todoList, setTodoList] = useState(() => {
         const savedTodoList = localStorage.getItem("savedTodoList");
         return savedTodoList ? JSON.parse(savedTodoList) : [];
@@ -11,10 +11,6 @@ const useSemiPersistentState = () => {
     useEffect(() => {
         localStorage.setItem("savedTodoList", JSON.stringify(todoList));
     }, [todoList]);
-    return [todoList, setTodoList];
-};
-function App() {
-    const [todoList, setTodoList] = useSemiPersistentState();
 
     const addTodo = (newTodo) => {
         setTodoList([...todoList, newTodo]);
