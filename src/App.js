e335cfb86158ed7bcede1715b7966ed7c358b04c
oxangyal <Oxana.mitch@gmail.com>
2023-06-24 18:45:48 -0400

    Fetch data from Airtable

--- src/App.js
+++ src/App.js
@@ -7,20 +7,37 @@ function App() {
     const [todoList, setTodoList] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve({
-                    data: {
-                        todoList:
-                            JSON.parse(localStorage.getItem("savedTodoList")) || []
-                    },
-                });
-            }, 2000);
-        }).then((result) => {
-            setTodoList(result.data.todoList);
+    async function fetchData() {
+        const options = {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
+            },
+        };
+
+        try {
+            const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}`;
+            const response = await fetch(url, options);
+            if (!response.ok) {
+                throw new Error(`Error: ${response.status}`);
+            }
+            const data = await response.json();
+            const todos = data.records.map((todo) => {
+                return {
+                    id: todo.id,
+                    title: todo.fields.title,
+                };
+            });
+
+            setTodoList(todos);
             setIsLoading(false);
-        });
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchData();
     }, []);
 
     useEffect(() => {
